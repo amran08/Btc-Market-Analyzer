be61bd17206038a8e4029034d65a97ffce259736
amran08 <raisulcse08@gmail.com>
2018-01-17 01:10:15 +0600

    Project Finalized

--- data_processor.js
+++ data_processor.js
@@ -1,4 +1,26 @@
 const api = require('./api_resolver');
+var api_key_config = require('./config.json');
+
+const BITTREX_API_KEY = api_key_config.bittrex_api_key;
+const CRYPTOPIA_API_KEY = api_key_config.cryptopia_api_key;
+const CRYPTOPIA_SECRET_KEY = api_key_config.cryptopia_api_secret;
+
+
+
+// configuration for cryptopia
+const Cryptopia = require('./cryptopia_api');
+const cryptopia = new Cryptopia(CRYPTOPIA_API_KEY, CRYPTOPIA_SECRET_KEY);
+
+// configuration for bittrex
+const bittrex = require('./node.bittrex.api');
+bittrex.options({ 
+  'apikey' : BITTREX_API_KEY, 
+  //'apisecret' : BITTREX_SECRET_KEY, 
+  'stream' : false, 
+  'verbose' : false, 
+  'cleartext' : false 
+});
+
 
 const stat_pending = 1;
 const stat_success = 2;
@@ -31,7 +53,7 @@ DATA_PROCESSOR.getOrderPrice = function(api_end_point,
                 var lastBid = obj.Data.Buy[0];
                 var newPrice = lastBid.Price * (1 + (percentage / 100));
                 var totalPrice = newPrice * lastBid.Volume;
-                callback(totalPrice);
+                callback(newPrice);
                 stat_callback(stat_success, "Success");
             }
             else
@@ -58,7 +80,7 @@ DATA_PROCESSOR.getOrderPrice = function(api_end_point,
                 var newRate = lastBid.Rate * (1 + (percentage / 100));
                 var volume = lastBid.Quantity / lastBid.Rate;
                 var newQuantity =  newRate * volume;
-                callback(newQuantity);
+                callback(newRate);
                 stat_callback(stat_success, "Success");
             }
             else
@@ -73,11 +95,57 @@ DATA_PROCESSOR.getOrderPrice = function(api_end_point,
     }
 }
 
+DATA_PROCESSOR.submitOrder = async function(market_name, api_end_point, btc_amount, order_price, callback, stat_callback)
+{
+    stat_callback(stat_pending, "Please wait...");
+    if(api_end_point.toLowerCase() == "cryptopia")
+    {
+        // call cryptopia end apis
+        try {
+            var data = await (cryptopia.SubmitTrade(market_name, null, "Buy", order_price, btc_amount));
+            if(data.Success == false)
+            {
+                // error occured
+                stat_callback(stat_failed, data.Error);
+            }
+            else
+            {
+                stat_callback(stat_success, data.Data);
+            }
+            
+        } catch (error) {
+            
+
+           stat_callback(stat_failed,"INVALID API KEY for CRYPTOPIA")
+            
+        }
+        
+        console.log(data);
+       
+    }
+    else{
+        // call bittrex end point
+        bittrex.buylimit('https://bittrex.com/api/v1.1/public/buylimit??apikey='+BITTREX_API_KEY+'&market='+market_name+'&quantity='+btc_amount+'&rate='+order_price, 
+        function(e, data) {
+           // console.log(data);
+            if(data.success == false)
+            {
+                stat_callback(stat_failed, data.message);
+            }
+            else
+            {
+                callback(data);
+                stat_callback(stat_success, data.message);
+            }
+        });
+    }
+}
 
 module.exports = {
     status_pending : stat_pending,
     status_success : stat_success,
     status_failed  : stat_failed,
 
-    get_order_price : DATA_PROCESSOR.getOrderPrice
+    get_order_price : DATA_PROCESSOR.getOrderPrice,
+    put_buy_order   : DATA_PROCESSOR.submitOrder
 }
--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const {app, BrowserWindow, Menu, ipcMain} = electron;
 const processor = require('./data_processor');
 
 let mainWindow;
+let apiKeyWindow;
+
+NODE_ENV=production;
 
 // listen for app to be ready
 app.on('ready', function(){
@@ -14,7 +17,7 @@ app.on('ready', function(){
     // create a new window
     mainWindow = new BrowserWindow({
         width : 800,
-        height : 280,
+        height : 300,
         resizable : false
     });
 
@@ -25,9 +28,35 @@ app.on('ready', function(){
         slashes : true
     }));
 
+    mainWindow.on('closed', function()
+    {
+        app.exit();
+    });
+
     let templateMenu = Menu.buildFromTemplate(menuTemplate);
     Menu.setApplicationMenu(templateMenu);
 });
+
+function showApiKeysWindow()
+{
+    // create a new window
+    apiKeyWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+    });
+
+    // load the html file
+    apiKeyWindow.loadURL(url.format({
+        pathname : path.join(__dirname, "apiKeyWindow.html"),
+        protocol : "file:",
+        slashes : true
+    }));
+
+    apiKeyWindow.on('closed', function()
+    {
+        apiKeyWindow = null;
+    });
+}
 
 ipcMain.on("process", function(e, market, api, percentage){
     console.log(market + " " + api + " " + percentage);
@@ -45,34 +74,57 @@ ipcMain.on("process", function(e, market, api, percentage){
                                 {
                                     case processor.status_pending:
                                     {
-                                        mainWindow.webContents.send("status", "#FEEFB3", "#9F6000", msg, true);
+                                        mainWindow.webContents.send("status", "#FEEFB3", "#9F6000", msg, true,false);
                                     }
                                     break;
                                     case processor.status_success:
                                     {
-                                        mainWindow.webContents.send("status", "#DFF2BF", "#4F8A10", msg, false);
+                                        mainWindow.webContents.send("status", "#DFF2BF", "#4F8A10", msg, false,true);
                                     }
                                     break;
                                     case processor.status_failed:
                                     {
-                                        mainWindow.webContents.send("status", "#FFD2D2","#D8000C", msg, false);
+                                        mainWindow.webContents.send("status", "#FFD2D2","#D8000C", msg, false,false);
                                     }
                                     break;
                                 }
                             });
-})
+});
+
+ipcMain.on("buy:bitcoin", function(e, market_name, api_end, btc_amount, order_price){
+    console.log(market_name + " : " + api_end + " : " + btc_amount + "  : " + order_price);
+
+    processor.put_buy_order(market_name, api_end, btc_amount, order_price, function(data)
+    {
+        console.log(data);
+    },
+    function(status, msg)
+    {
+        switch(status)
+        {
+            case processor.status_pending:
+            {
+                mainWindow.webContents.send("status", "#FEEFB3", "#9F6000", msg, true,false);
+            }
+            break;
+            case processor.status_success:
+            {
+                mainWindow.webContents.send("status", "#DFF2BF", "#4F8A10", msg, false,true);
+            }
+            break;
+            case processor.status_failed:
+            {
+                mainWindow.webContents.send("status", "#FFD2D2","#D8000C", msg, false,false);
+            }
+            break;
+        }
+    });
+});
 
 const menuTemplate = [
     {
         label: 'File',
         submenu:[
-            // {
-            //     label : 'Clear Fields',
-            //     click(){
-            //         console.log("Clear fields called");
-            //         mainWindow.webContents.send('clear:fields');
-            //     }
-            // },
             {
                 label : 'Exit',
                 click()
